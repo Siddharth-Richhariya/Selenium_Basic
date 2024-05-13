520f3c5db76d6b9c27a3053df68534984415b5e7
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-05-14 00:04:04 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Main {
+    static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.browserstack.com/guide/maven-dependency-with-selenium");
+        Thread.sleep(1000);
+        driver.close();
+    }
+}
