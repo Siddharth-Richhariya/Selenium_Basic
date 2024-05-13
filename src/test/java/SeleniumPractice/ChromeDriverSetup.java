d12f54dfb6820183c868508163f710d4fb9a3d80
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-05-14 00:25:28 +0530

    Chrome

--- src/test/java/SeleniumPractice/ChromeDriverSetup.java
+++ src/test/java/SeleniumPractice/ChromeDriverSetup.java
@@ -1,16 +1,17 @@
-package org.example;
+package SeleniumPractice;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static io.github.bonigarcia.wdm.WebDriverManager.*;
 
-public class Main {
+
+public class ChromeDriverSetup {
     static WebDriver driver;
     public static void main(String[] args) throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
+        chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://www.browserstack.com/guide/maven-dependency-with-selenium");
+        driver.get("https://www.google.com/");
         Thread.sleep(1000);
         driver.close();
     }
