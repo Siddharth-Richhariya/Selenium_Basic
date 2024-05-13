e96e798c9a47552eaa7042ac39f4a68adb2d7428
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-05-14 00:27:09 +0530

    WebDriver Manager Setup With Static

--- src/test/java/SeleniumPractice/ChromeDriverSetup.java
+++ src/test/java/SeleniumPractice/ChromeDriverSetup.java
@@ -2,9 +2,7 @@ package SeleniumPractice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import static io.github.bonigarcia.wdm.WebDriverManager.*;
-
 
 public class ChromeDriverSetup {
     static WebDriver driver;
