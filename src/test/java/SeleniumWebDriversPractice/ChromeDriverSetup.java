2c79ecddcf861b44f03323e1af21de4a8cd0ad2f
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-05-14 23:59:29 +0530

    Chrome Setup

--- src/test/java/SeleniumWebDriversPractice/ChromeDriverSetup.java
+++ src/test/java/SeleniumWebDriversPractice/ChromeDriverSetup.java
@@ -2,15 +2,28 @@ package SeleniumWebDriversPractice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.net.SocketException;
+
 import static io.github.bonigarcia.wdm.WebDriverManager.*;
 
 public class ChromeDriverSetup {
     static WebDriver driver;
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        Thread.sleep(1000);
-        driver.close();
+
+        try {
+            driver.manage().window().maximize();
+            driver.get("https://www.google.com/search?q=chuchi");
+
+            Thread.sleep(1000);
+            driver.close();
+        } catch(InterruptedException e) {
+            System.out.println("Interrupted Exception");
+        }
+
+
     }
 }
